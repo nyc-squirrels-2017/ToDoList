bd7b68e88c198130c8a0fb61788d463a4f445634
Adedotun Oni <email": null>
2017-02-06 18:09:41 -0500

    Failed attempt to ajaxify my code

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -4,14 +4,19 @@ end
 
 
 post '/session/login'do
-user = User.find_by(username: params[:user][:username])
-  if user && user.authenticate(params[:user][:password])
-    session[:user_id] = user.id
-    redirect '/'
-  else
-  @errors = ['Something went Wrong.']
-  erb :'/session/login'
-  end
+  if request.xhr?
+  user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      erb :'/paritials/_sign_up_form', layout: false
+    else
+      erb :'/session/login'
+    end
+    else
+    @errors = ['Something went Wrong.']
+    erb :'/session/login'
+    end
+
 end
 
 
